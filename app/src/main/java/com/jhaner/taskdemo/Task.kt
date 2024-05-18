a84bde01547e88a7d7c0ce44100c0e361cfcef57
Jhonnatan Espinoza Rojas <jhonnatan.espinoza.r@uni.pe>
2024-05-18 18:13:20 -0500

    tarea en task.kt

--- app/src/main/java/com/jhaner/taskdemo/Task.kt
+++ app/src/main/java/com/jhaner/taskdemo/Task.kt
@@ -1,8 +1,6 @@
 package com.jhaner.taskdemo
 
 // data class <- clase que se utiliza para almacenar datos
-data class Task(
-    val id: Int = 0,
-    val name: String,
-    val isCompleted: Boolean = false
-)
+// TODO 1: Crear una data class llamada Task que tenga tres propiedades: id, name y isCompleted
+// ej. data class User(val id: Int, val name: String, val age: Int)
+data class Task()
